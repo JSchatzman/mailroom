266237e0982a76fba741ec364077f0b6fd056ec5
jordan <j.schatzman@outlook.com>
2016-12-08 15:26:26 -0800

    adding new donors to list

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,4 +1,5 @@
 """Mailroom Implementation.  Create thank you cards or donation reports."""
+from __future__ import unicode_literals
 
 donors = {'jordan schatzman': [100, 200, 400, 50.6],
           'rick valenzuela': [500, 300, 100, 50, 1.24],
@@ -31,11 +32,29 @@ def send_thank_you():
     if choice.lower() == 'list':
         for donor in sorted(list(donors.keys())):
             donor = donor.split(' ')
+            # The line below breaks prints capitalized first/last name
             print (' '.join(list(map(lambda x: x.capitalize(), donor))))
     elif choice.lower == 'menu':
         choose_path()
     else:
-        pass 
+        if choice.lower() in donors:
+            print ('{0} has already donated.  Enter new donation.'.format(choice))
+            add_donation(choice.lower())
+        else:
+            print ('{0} has been added.  Enter new donation.'.format(choice))
+            donors[choice.lower()] = []
+            add_donation(choice.lower())
+
+def add_donation(donor):
+    """Add donation for existing donor,"""
+    input_prompt = 'How much is the new donation worth?'
+    donation = input(input_prompt)
+    while not donation.isnumeric():
+        input_prompt = 'Please enter a valid number'
+        donation = input(input_prompt)
+    donors[donor].append(donation)
+
+
 
 
 def create_a_report():
