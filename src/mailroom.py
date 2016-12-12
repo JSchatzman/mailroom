0468aa16a585f3673670c9f10e09230f5f55bd0c
Rick Valenzuela <devnull@rickv.com>
2016-12-11 21:19:38 -0800

    pulled jordan's file, added quit and loop from list to control_thank_you

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,105 +1,104 @@
 """Mailroom Implementation.  Create thank you cards or donation reports."""
 from __future__ import unicode_literals
-import math
 
-donors = {'jordan schatzman': [100, 200, 400, 50.6],
+donors = {'jordan schatzman': [200, 240, 45, 2, 1000],
           'rick valenzuela': [500, 300, 100, 50, 1.24],
           'sally johnson': [50, 100, 1, 2, 99],
           'jane': [39, 23, 532, 2432],
           'bob': [89, 23, 12, 3.45643]}
 
 
-def choose_path():
+def main():
     """Allow the user to begin the program."""
-    print('If you want to submit a thank you, enter:')
-    print('[T] to send a [T]hank you')
-    print('[R] to show a donation [R]eport')
-    print('If at any time you wish to return to this menu,')
-    print('enter "menu".  Otherwise, enter "Quit" to quit')
-    input_prompt = "> "
-    valid = True
-    while valid:
+    while True:
+        input_prompt = 'If you want to submit a thank you, enter'
+        input_prompt += ' "Send a Thank You".  If you would like to see a'
+        input_prompt += ' donation report, enter "Create a Report".'
+        input_prompt += ' If at any time you wish to return to this menu,'
+        input_prompt += ' enter "menu".  Otherwise, enter "Quit" to quit: '
         choice = input(input_prompt)
-        if choice.lower() == 'r':
-            create_a_report()
-        elif choice.lower() == 't':
-            get_donor()
+        if choice.lower() == 'create a report':
+            print (create_a_report(donors))
+        elif choice.lower() == 'send a thank you':
+            control_thank_you()
         elif choice.lower() == 'quit':
             exit()
         else:
             print ('\n Please submit a valid input. \n')
-            choose_path()
-
-choice = ""
-donation = ""
+            main()
 
 
-def get_donor():
-    """Create donor to pass to send_thank_you()."""
-    global choice
-    input_prompt = 'If you want to see a list of donors, enter "list".'
-    input_prompt += '  Otherwise, enter a donor name: '
-    choice = input(input_prompt)
-    if choice.lower() == 'menu':
-        choose_path()
-    elif choice.lower() == 'quit':
-        exit()
-    elif choice.lower() == 'list':
-        for donor in sorted(list(donors.keys())):
-            donor = donor.split(' ')
-            # The line below breaks prints capitalized first/last names
-            print (' '.join(list(map(lambda x: x.capitalize(), donor))))
-        get_donor()
-    get_donation_amt()
+def control_thank_you():
+    """Handle logic for sending a thank you."""
+    while True:
+        input_prompt = 'If you want to see a list of donors, enter "list".'
+        input_prompt += '  Otherwise, enter a donor name: '
+        choice = input(input_prompt)
+        if choice == 'list':
+            sorted_donors = create_sorted_list(donors)
+            for donor in sorted_donors:
+                print (donor)
+        elif choice == 'menu':
+            print ('\n \n')
+            main()
+        elif choice == 'quit':
+            exit()
+        elif choice.lower() in donors:
+            chosen_donor = choice.lower()
+            input_prompt = choice + ' has already donated.'
+            input_prompt += '  Please enter a new donation:  '
+            donation = input(input_prompt)
+            new_donation = add_donation(donors, chosen_donor, donation)
+            print (print_email(donors, chosen_donor, new_donation))
+            main()
+        elif choice.lower() not in donors:
+            chosen_donor = choice.lower()
+            donors[chosen_donor] = []
+            input_prompt = chosen_donor + ' has been added.  Please'
+            input_prompt += ' submit a donation:  '
+            donation = input(input_prompt)
+            new_donation = add_donation(donors, chosen_donor, donation)
+            print (print_email(donors, chosen_donor, new_donation))
+            main()
 
 
-def get_donation_amt():
-    """Create donation amount variable."""
-    global donation
-    print("Enter donation amount: ")
-    donation = input("> ")
-    while not isinstance(donation, int) or not isinstance(donation, float):
-        input_prompt = 'Please enter a valid number: '
-        donation = input(input_prompt)
-
-    print(donors)
-    add_donation()
-    print(donors)
+def create_sorted_list(donors):
+    """Create a sorted list of donors."""
+    sorted_list = []
+    for donor in sorted(map(lambda x: x.capitalize(), list(donors.keys()))):
+        donor = donor.split(' ')
+        sorted_list.append(' '.join(map(lambda x: x.capitalize(), donor)))
+    return sorted_list
 
 
-def send_thank_you():
-    """Create a report with full donor list with name and donation stats."""
-    donation = add_donation(choice.lower())
-    print_email(choice, donation)
+def add_donation(donors, donor, donation):
+    """Add donation for a speciic donor."""
+    invalid_donation = True
+    while invalid_donation:
+        try:
+            invalid_donation = float(donation)
+            donors[donor].append(float(donation))
+            invalid_donation = False
+        except ValueError:
+            input_prompt = 'Please enter a valid number: '
+            donation = input(input_prompt)
+    return donation
 
 
-def add_donation():
-    """Add donation for existing donor."""
-    if choice.lower() in donors:
-        print("{} has already donated. Adding new donation.".format(choice))
-        donors[str(choice)].append(float(donation))
-        print_email(choice, donation)
-    else:
-        print ('Adding {choice} and donation amount to the donors list.')
-        donors[choice.lower()] = [donation]
-        print_email(choice, donation)
-
-
-def print_email(donor, donation):
+def print_email(donors, donor, donation):
     """Print email to console."""
-    email = "Dear {0},"
-    email += '\n \n \n'
+    donor_capital = ' '.join(map(lambda x: x.capitalize(), donor.split(' ')))
+    email = "Dear {0},\n \n \n"
     email += 'Thanks so much for your donation of ${1}.  We love you!'
     email += ' Please feel free to donate again.'
-    email += '\n \n \n'
-    email += 'Love, \n CodeFellows'
-    print (email.format(donor, donation))
+    email += 'Love, \n CodeFellows\n\n\n'
+    return email.format(donor_capital, donation)
 
 
-def create_a_report():
-    """Print donation report, with total, count, average."""
-    print ('Donor Name--------------Count-----Average--------Sum')
-    print ('-' * 62)
+def create_a_report(donors):
+    """Create a donor report with list of donors, count, average and sum."""
+    output = 'Donor Name--------------Count-----Average--------Sum\n'
+    output += ('-' * 62) + '\n'
     donor_ordered = [[k, sum(v)] for k, v in donors.items()]
     donor_ordered.sort(key=lambda x: -x[1])
     for donor in donor_ordered:
@@ -110,15 +109,16 @@ def create_a_report():
         name = donor[0].split(' ')
         name = ' '.join(list(map(lambda x: x.capitalize(), name)))
 
-        print ('{0}{1}{2}{3}{4}{5}{6}{7}'.format(' ' * 3,
-                                                 name,
-                                                 ' ' * (23 - len(name)),
-                                                 str(len(donations)),
-                                                 ' ' * (10 - len(donation_count)),
-                                                 donation_avg,
-                                                 ' ' * (13 - len(donation_avg)),
-                                                 donation_sum))
-    choose_path()
+        output += '{0}{1}{2}{3}{4}{5}{6}{7}\n'.format(' ' * 3,
+                                                      name,
+                                                      ' ' * (23 - len(name)),
+                                                      str(len(donations)),
+                                                      ' ' * (10 - len(donation_count)),
+                                                      donation_avg,
+                                                      ' ' * (13 - len(donation_avg)),
+                                                      donation_sum)
+    return output
+
 
 if __name__ == '__main__':
-    choose_path()
+    main()
